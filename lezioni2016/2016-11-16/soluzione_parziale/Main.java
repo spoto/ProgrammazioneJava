ac72b6685a6fc8d4623247f5e63205f84c0ca197
Fausto Spoto <fausto.spoto@univr.it>
2016-11-16 11:17:47 +0100

    Aggiunta la soluzione parziale di oggi

---
+++ lezioni2016/2016-11-16/soluzione_parziale/Main.java
@@ -0,0 +1,9 @@
+import it.univr.gioco15.Gioco15;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Gioco15 gioco15 = new Gioco15();
+		System.out.println(gioco15.toString());
+	}
+}
